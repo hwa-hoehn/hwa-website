13baa6f3e976786a12bb9d030c1e47243974f066
veit910 <veit.hoehn@gmx.de>
2026-09-04 16:20:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ if (form) {
   const reduceMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
   if (reduceMotion) return;
 
-  const phrases = ['Vereine', 'Handwerker', 'Gastronomen','Vermieter','Friseure', 'Beauty-Studios','Werkstätten','Bäckereien','Ladenbesitzer','Selbstständige'];
+  const phrases = ['Handwerker','Gastronomen','Vermieter','Friseure', 'Beauty-Studios','Werkstätten','Bäckereien','Ladenbesitzer','Selbstständige','Vereine'];
   let i = 0;
 
   setInterval(() => {
